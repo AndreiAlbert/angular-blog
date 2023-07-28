f146c7bcfbfe595a65d53d8c2c5532c21471251e
Andrei Albert Havirneanu <albert.havirneanu@connatix.com>
2023-07-28 10:13:35 +0300

    added redirect after reading an article

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ArticlesServiceService } from 'src/services/articles-service.service';
 import { Article } from 'src/utils/Article';
 
@@ -15,13 +16,14 @@ export class AddComponent {
     content: '',
   };
 
-  constructor(private articleService: ArticlesServiceService) {}
+  constructor(private articleService: ArticlesServiceService, private router: Router) {}
 
    onSubmit() {
     this.articleService.addArticle(this.newArticle).subscribe(
       (addedArticle) => {
         console.log('Article added successfully:', addedArticle);
         this.resetForm();
+        this.router.navigate(['articles']);
       },
       (error) => {
         console.error('Failed to add article:', error);
